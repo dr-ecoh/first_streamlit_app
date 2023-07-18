7aac323643609f3e35ade772e051c0bd56947c77
dr-ecoh <41723953+dr-ecoh@users.noreply.github.com>
2023-07-18 17:10:44 +0200

    convert json to table

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,4 +26,10 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+# just writes the data to the screen
 streamlit.text(fruityvice_response.json())
+
+# take the json version of the response and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output it as a table
+streamlit.dataframe(fruityvice_normalized)
