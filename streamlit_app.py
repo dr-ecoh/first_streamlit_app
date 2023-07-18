40a681676ea8b4114d0435a9afc8c1b938695a31
dr-ecoh <41723953+dr-ecoh@users.noreply.github.com>
2023-07-18 17:08:02 +0200

    converted API response to JSON

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,5 +23,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # New section to display FruityVice API response
+streamlit.header("Fruityvice Fruit Advice!")
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
